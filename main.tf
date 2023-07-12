ce880e1c2996c555b3f5fa4070a1698afe5a69e3
shinter61 <shintaro-matsumoto@c-fo.com>
2023-07-12 14:13:24 +0900

    Set terraform backend s3

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket = "sre-onboarding-shinta-tfstate"
+    key = "terraform.tfstate"
+    region = "ap-northeast-1"
+    encrypt = true
+    profile = "saml"
+  }
+}
+
 provider "aws" {
   region  = "ap-northeast-1"
   profile = "saml"
