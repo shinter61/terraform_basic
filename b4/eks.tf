bd1ad4d3c39a76ee3721dad8a3f3c68c5b614bd9
shinter61 <shintaro-matsumoto@c-fo.com>
2023-07-14 15:52:45 +0900

    Create eks cluster

--- b4/eks.tf
+++ b4/eks.tf
@@ -5,35 +5,29 @@ module "eks" {
   cluster_name = "sre-onboarding-shinta-tf"
   cluster_version = "1.27"
 
+  enable_irsa = true
+  vpc_id = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
   # kubectl で Kubernetes API サーバーと通信するため
   # ref: https://docs.aws.amazon.com/ja_jp/eks/latest/userguide/cluster-endpoint.html
   cluster_endpoint_public_access = true
-  cluster_endpoint_private_access = false
+  # cluster_endpoint_private_access = false
 
-  # cluster_endpoint_public_access_cidrs = [var.office_vpn]
-
-  vpc_id = module.vpc.vpc_id
-  subnet_ids = concat(module.vpc.public_subnets, module.vpc.private_subnets)
-
-  # Default では Kubernetes コントロールプレーンは AWSの管理するVPC内に作成される
-  # control_plane_subnet_ids = []
-
-  self_managed_node_group_defaults = {
+  eks_managed_node_group_defaults = {
     iam_role_additional_policies = {
       AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
     }
+
+    instance_types = ["m5.large"]
   }
 
-  self_managed_node_groups = {
-    sre-onboarding-shinta-tf = {
+  # self_managed_node_groups = {
+  eks_managed_node_groups = {
+    main = {
       desired_size = 3
       max_size     = 3
       min_size     = 3
-      instance_type    = "m5.large"
-      # v1.27 EKS optimized AMI
-      # ref: https://docs.aws.amazon.com/ja_jp/eks/latest/userguide/retrieve-ami-id.html
-      ami_id = "ami-0a6fc0cbf0a8433cb"
-      subnet_ids          = module.vpc.public_subnets
     }
   }
 }
--- b4/vpc.tf
+++ b4/vpc.tf
@@ -9,14 +9,12 @@ module "vpc" {
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway     = true
-  single_nat_gateway     = true
 
-  map_public_ip_on_launch = true
-
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-
-  tags = {
-    hoge = "fuga"
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = "1"
+  }
+ 
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = "1"
   }
 }
